92785fcf1c63aa904034a472bbee979df55b3eb1
Hanen Zeddini <hanen.zeddini@gmail.com>
2023-10-23 10:47:59 -0700

    Final Change

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,38 +1,56 @@
 import os
 import csv
+# Initialize the variable to store net profit
 Net_Profit = 0
 
-# To Open the CSV file  
+# Define the path to the CSV file
 csvpath = os.path.join('Resources/budget_data.csv')
+
+# Open the CSV file
 with open (csvpath) as csv_file:  
-        csv_read = csv.reader( csv_file)  
-    
+        csv_read = csv.reader( csv_file) 
+
+        # Read the CSV data and extract headers 
         Data = list(csv_read)
         Headers = Data[0]
+
+        # Remove headers from the data
         Data.pop(0) 
 
+         # Calculate the total number of months
         Total_Months = len(Data)
        
-#############        
+#############   
+# Calculate the net profit by summing up all the profits/losses     
 for i in range (0, len(Data)):
     Net_Profit += int(Data[i][1])
 
 ############
+# Initialize variables for calculating changes
 Changes = 0
+
+
+# Calculate the sum of changes between months
 for i in range (0,len(Data)-1):
     Changes += int(Data[i+1][1]) - int(Data[i][1])
+
+ # Calculate the average change   
 Avg_changes = Changes / (len (Data)-1) 
 
 ############
+
+# Initialize variables to track the greatest increase and decrease in profits
+Greatest_Increase = 0
 Greatest_Increase = 0
 Date_Increase =""
+
+# Find the greatest increase and decrease in profits
 for i in range (0,len(Data)-1):
     Increase = int(Data[i+1][1]) - int(Data[i][1])
     if Increase > Greatest_Increase:
         Greatest_Increase = Increase
         Date_Increase = Data[i+1][0]
 
-############
 Greatest_Decrease = 0
 Date_Decrease =""
 for i in range (0,len(Data)-1):
@@ -42,6 +60,8 @@ for i in range (0,len(Data)-1):
         Date_Decrease = Data[i+1][0]
       
 ##############
+
+# Prepare the output lines  
 line1 = "Financial Analysis"
 line2 = "----------------------------"
 line3 = "Total Months: {}".format(Total_Months)
@@ -49,6 +69,8 @@ line4 = "Total: ${}".format(Net_Profit)
 line5 = "Average Change: ${:.2f}".format(Avg_changes)
 line6 = "Greatest Increase in Profits: {} (${})".format(Date_Increase,Greatest_Increase)
 line7 = "Greatest Decrease in Profits: {} (${})".format(Date_Decrease,Greatest_Decrease)
+
+# Print the results to the console
 print(line1)
 print(line2)
 print(line3)
@@ -57,6 +79,8 @@ print(line5)
 print(line6)
 print(line7)
 #############
+
+# Write the results to a text file
 with open("./analysis/analysis.txt", "w") as file:
      file.write(line1+"\n")
      file.write(line2+"\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,19 +1,24 @@
 import os
 import csv
 
-
-# To Open the CSV file  
+# Define the path to the CSV file
 csvpath = os.path.join('Resources/election_data.csv')
+
+# Open the CSV file
 with open (csvpath) as csv_file:  
+    # Read the CSV data        
         csv_read = csv.reader( csv_file)  
-    
+    # Convert the CSV data into a list
         Data = list(csv_read)
+    # Extract the headers and remove them from the data    
         Headers = Data[0]
         Data.pop(0) 
-
+    # Calculate the total number of votes
         Total_Votes = len(Data)
         
-################
+#################
+
+# Create a dictionary to store candidate votes
 
 Candidates = {}
 for vote in Data:
@@ -23,9 +28,13 @@ for vote in Data:
         Candidates[vote[2]]=1
 
 #################
+
+# Find the winner and their total votes
 winner = ""
 max_vote = 0
 candidate_print = []
+
+# Calculate the percentage of votes for each candidate and determine the winner
 for c,v in Candidates.items():
     candidate_print.append("{}: {:.3f}% ({})".format(c,v/Total_Votes*100,v))
 
@@ -34,10 +43,14 @@ for c,v in Candidates.items():
         winner = c
       
 ##################
+
+# Prepare the output lines
 line1 = "Election Results"
 line2 = "-------------------------"
 line3 = "Total Votes: {}".format(Total_Votes)
 line4 = "Winner: {}".format(winner)
+
+# Print the results to the console
 print(line1)
 print(line2)
 print(line3)
@@ -48,6 +61,8 @@ print(line2)
 print(line4)
 print(line2)
 ###############
+
+# Write the results to a text file
 with open("./analysis/analysis.txt", "w") as file:
     file.write(line1+"\n")
     file.write(line2+"\n")
